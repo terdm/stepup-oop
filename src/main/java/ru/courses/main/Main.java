191256de7b5a799d26c745f40f268732e5dbbe9b
DTerentev <terdm@yandex.ru>
2025-08-31 18:50:48 +0300

    two points, one by simple name

--- src/main/java/ru/courses/main/Main.java
+++ src/main/java/ru/courses/main/Main.java
@@ -1,45 +1,19 @@
 package ru.courses.main;
 
-// Импорты для использования коротких имен методов
-import static java.lang.Integer.parseInt;
-import static java.lang.Math.pow;
+// Используем простое имя для нашего класса
+import ru.courses.geometry.Point;
 
 public class Main {
-
-    /**
-     * Метод для возведения числа X в степень Y
-     * @param xStr строка с числом X
-     * @param yStr строка с числом Y
-     * @return результат возведения X в степень Y
-     */
-    public static double power(String xStr, String yStr) {
-        // Преобразуем строки в числа используя короткое имя метода
-        int x = parseInt(xStr);
-        int y = parseInt(yStr);
-
-        // Возводим в степень используя короткое имя метода
-        return pow(x, y);
-    }
-
     public static void main(String[] args) {
-        // Проверяем, что передано достаточно аргументов
-        if (args.length < 2) {
-            System.out.println("Использование: java Main <число> <степень>");
-            System.out.println("Пример: java Main 2 3");
-            return;
-        }
+        // Наш класс - по простому имени
+        Point geometryPoint = new Point(3, 4);
 
-        try {
-            // Вызываем метод power с параметрами командной строки
-            double result = power(args[0], args[1]);
+        // AWT класс - по полному имени
+        java.awt.Point awtPoint = new java.awt.Point(10, 20);
 
-            // Выводим результат
-            System.out.println(args[0] + " в степени " + args[1] + " = " + result);
-
-        } catch (NumberFormatException e) {
-            System.out.println("Ошибка: оба аргумента должны быть целыми числами");
-        } catch (Exception e) {
-            System.out.println("Произошла ошибка: " + e.getMessage());
-        }
+        System.out.println("Точка из ru.courses.geometry: " + geometryPoint);
+        System.out.println("Точка из java.awt: " + awtPoint);
+        System.out.println("Тип geometryPoint: " + geometryPoint.getClass().getName());
+        System.out.println("Тип awtPoint: " + awtPoint.getClass().getName());
     }
 }
