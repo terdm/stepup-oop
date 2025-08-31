e61e3208188680a5e6b2160060e5f02fbce5cb3b
DTerentev <terdm@yandex.ru>
2025-08-31 17:32:04 +0300

    added Main class

---
+++ src/main/java/ru/courses/main/Main.java
@@ -0,0 +1,8 @@
+package ru.courses.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Программа запущена из пакета ru.courses.main");
+        // Здесь будет основной код вашей программы
+    }
+}
